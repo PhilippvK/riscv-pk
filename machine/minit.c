54b1264e7f9f306d19c7e0fc6828289dccd1fda5
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-11-25 14:22:55 +0100

    workaround to support support vector calling convention in gcc 14+

--- machine/minit.c
+++ machine/minit.c
@@ -44,7 +44,7 @@ static void mstatus_init()
     mstatus |= MSTATUS_FS;
 
   // Enable vector extension
-  if (supports_extension('V'))
+  // if (supports_extension('V'))
     mstatus |= MSTATUS_VS;
 
   write_csr(mstatus, mstatus);
@@ -191,6 +191,7 @@ static void wake_harts()
 
 void init_first_hart(uintptr_t hartid, uintptr_t dtb)
 {
+  mstatus_init();
   // Confirm console as early as possible
   query_uart(dtb);
   query_uart16550(dtb);
